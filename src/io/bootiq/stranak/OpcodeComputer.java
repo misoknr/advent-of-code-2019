da4bc7cc22d432eb8ff76d3ba11fec6a45ca76a1
Michal Stranak <michal.stranak@bootiq.io>
2019-12-27 17:17:18 +0100

    Day 5 - Part 1

--- src/io/bootiq/stranak/OpcodeComputer.java
+++ src/io/bootiq/stranak/OpcodeComputer.java
@@ -1,14 +1,16 @@
 package io.bootiq.stranak;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class OpcodeComputer {
 
-    public Integer[] readMemory() throws FileNotFoundException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("input/opcode_input.txt"));
+    private List<Integer> output = new ArrayList<>();
+
+    public Integer[] readMemory(String inputFile) throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("input/" + inputFile));
         var optionalString = bufferedReader.lines().reduce((s1, s2) -> s1.concat(s2));
 
         if (optionalString.isPresent()) {
@@ -18,23 +20,61 @@ public class OpcodeComputer {
         return null;
     }
 
-    private int executeInstruction(Integer[] memory, int memoryPointer) {
-        int instruction = memory[memoryPointer];
+    private int executeInstruction(Integer[] memory, int memoryPointer) throws IOException {
+        String operation = String.valueOf(memory[memoryPointer]);
+        int operationLength = operation.length();
+        int instruction = Integer.valueOf(operation.substring(operationLength > 2 ? operation.length() - 2 : 0));
+        int[] parameterModes = new int[]{0, 0, 0};
+
+        if (operation.length() > 2) {
+            String modes = operation.substring(0, operation.length() - 2);
+            int modePosition = 0;
+
+            for (int i = modes.length() - 1; i >= 0; i--) {
+                parameterModes[modePosition] = Integer.parseInt(modes.substring(i, i + 1));
+                modePosition++;
+            }
+        }
+
+        int param1;
+        int param2;
+        int writePointer;
 
         switch (instruction) {
             case 1:
-                memory[memory[memoryPointer + 3]] = memory[memory[memoryPointer + 1]] + memory[memory[memoryPointer + 2]];
+                param1 = parameterModes[0] == 0 ? memory[memory[memoryPointer + 1]] : memory[memoryPointer + 1];
+                param2 = parameterModes[1] == 0 ? memory[memory[memoryPointer + 2]] : memory[memoryPointer + 2];
+                writePointer = memory[memoryPointer + 3];
+                memory[writePointer] = param1 + param2;
+
                 return memoryPointer + 4;
             case 2:
-                memory[memory[memoryPointer + 3]] = memory[memory[memoryPointer + 1]] * memory[memory[memoryPointer + 2]];
+                param1 = parameterModes[0] == 0 ? memory[memory[memoryPointer + 1]] : memory[memoryPointer + 1];
+                param2 = parameterModes[1] == 0 ? memory[memory[memoryPointer + 2]] : memory[memoryPointer + 2];
+                writePointer = memory[memoryPointer + 3];
+                memory[writePointer] = param1 * param2;
+
                 return memoryPointer + 4;
+            case 3:
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                int input = Integer.parseInt(reader.readLine());
+                param1 = memory[memoryPointer + 1];
+                memory[param1] = input;
+
+                return memoryPointer + 2;
+            case 4:
+                param1 = memory[memoryPointer + 1];
+                this.output.add(memory[param1]);
+
+                return memoryPointer + 2;
             case 99:
             default:
                 return -1;
         }
     }
 
-    public void processMemory(Integer[] memory, Integer noun, Integer verb) {
+    public void processMemory(Integer[] memory, Integer noun, Integer verb) throws IOException {
+        this.output.clear();
         int position = 0;
 
         if (noun != null) {
@@ -50,7 +90,7 @@ public class OpcodeComputer {
         } while (position >= 0);
     }
 
-    public int[] findNounAndVerbForOutput(Integer[] memory, int targetOutput) {
+    public int[] findNounAndVerbForOutput(Integer[] memory, int targetOutput) throws IOException {
         int[] foundWords = null;
 
         for (int noun = 0; noun < 100; noun++) {
@@ -73,22 +113,35 @@ public class OpcodeComputer {
         return foundWords;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         OpcodeComputer opcodeComputer = new OpcodeComputer();
-        Integer[] memory = opcodeComputer.readMemory();
-        Integer[] memoryCopy = memory.clone();
+        Integer[] memory = opcodeComputer.readMemory("opcode2_input.txt");
+//        Integer[] memory = new Integer[]{3, 0, 4, 0, 99};
 
+//        Integer[] memoryCopy = memory.clone();
 //        opcodeComputer.processMemory(memoryCopy, 12, 2);
 //        System.out.println("Value at memory zero position: " + memoryCopy[0]);
 
-        int targetOutput = 19690720;
-        int[] words = opcodeComputer.findNounAndVerbForOutput(memory, targetOutput);
+//        int targetOutput = 19690720;
+//        int[] words = opcodeComputer.findNounAndVerbForOutput(memory, targetOutput);
+//
+//        if (words == null || words.length < 2) {
+//            System.out.println(String.format("Noun and verb that produces output '%d' not found", targetOutput));
+//        } else {
+//            System.out.println(String.format("Found noun and verb for output %d: [%d, %d]", targetOutput, words[0], words[1]));
+//            System.out.println("100 * NOUN + VERB = " + (100 * words[0] + words[1]));
+//        }
 
-        if (words == null || words.length < 2) {
-            System.out.println(String.format("Noun and verb that produces output '%d' not found", targetOutput));
-        } else {
-            System.out.println(String.format("Found noun and verb for output %d: [%d, %d]", targetOutput, words[0], words[1]));
-            System.out.println("100 * NOUN + VERB = " + (100 * words[0] + words[1]));
+        opcodeComputer.processMemory(memory, null, null);
+
+        for (int i = 0; i < memory.length; i++) {
+            System.out.print(memory[i] + ", ");
+        }
+
+        System.out.println("Output: ");
+
+        for (Integer o : opcodeComputer.output) {
+            System.out.print(o + ",");
         }
     }
 
